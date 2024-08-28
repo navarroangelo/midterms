bf7ed6152f76caf1780f8fe00cb13b837f1c7d50
sjh-706 <sjh-706@sjh-706-31.local>
2024-08-28 10:21:24 +0800

    Integration Current State: To Be Improved

--- static/index.js
+++ static/index.js
@@ -2,9 +2,15 @@ const multer = require('multer');
 const express = require('express');
 const app = express();
 
+// BODY PARSER REQUIREMENT
+const bodyParser = require('body-parser');
+
+// APPLICATION/X-WWW-FORM-URLENCODED PARSER
+const urlencodedparser = bodyParser.urlencoded({ extended: false })
+
 app.use(express.static('public'));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFIle(__dirname + '/' + index.html);
 });
 
@@ -17,7 +23,7 @@ app.get('/blog', (req, res) => {
 });
 
 app.get('/upload', (req, res) => {
-    res.sendFIle(__dirname + '/' + index.html);
+    res.sendFIle(__dirname + '/' + upload.html);
 });
 
 app.get('/contact', (req, res) => {
@@ -26,6 +32,73 @@ app.get('/contact', (req, res) => {
 
 app.get('/profile', (req, res) => {
     res.sendFIle(__dirname + '/' + profile.html);
+});
+
+// POST
+app.post('/formPost', urlencodedparser, (req, res) => {
+    // OUTPUT IN JSON FORMAT
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        id: req.body.worker_id,
+        email: req.body.email,
+        phone: req.body.phone,
+        department: req.body.department
+    };
+
+    // OTHER FORMATTING
+    const htmlPost =
+        `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <style>
+                body {
+                    background-color: whitesmoke;
+                    text-align: center;
+                    padding: 1em;
+                }
+                p {
+                    text-align: left;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>User Information Summary</h1>
+            <p>
+                <strong>
+                    First Name:
+                </strong> ${response.first_name}
+            </p>
+            <p>
+                <strong>
+                    Last Name:
+                </strong> ${response.last_name}
+            </p>
+            <p>
+                <strong>
+                    Worker ID:
+                </strong> ${response.worker_id}
+            </p>
+            <p>
+                <strong>
+                    E-Mail:
+                </strong> ${response.email}
+            </p>
+            <p>
+                <strong>
+                    Phone:
+                </strong> ${response.phone}
+            </p>
+            <p>
+                <strong>
+                    Department:
+                </strong> ${response.department}
+            </p>
+        </body>
+        </html>`
+
+    console.log(htmlPost);
+    res.end(htmlPost);
 });
 
 const port = process.env.PORT || 4001;
